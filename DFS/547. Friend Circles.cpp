063835aefda9bd6a34d9e3d2bc0739a81f71d42b
侯安康 <1611506326@qq.com>
2019-06-02 21:06:15 +0800

    朋友圈

--- DFS/547. Friend Circles.cpp
+++ DFS/547. Friend Circles.cpp
@@ -27,9 +27,17 @@ M[i][i] = 1 for all students.
 If M[i][j] = 1, then M[j][i] = 1.
 解析：给定一个N阶方阵m m[i][j]=1代表i和j是朋友关系，朋友关系可传递，即若0和1是朋友，1和2是朋友，那么0和2是朋友；又0和1是朋友，0和2朋友，那么
 1和2是朋友。互相为朋友的同学在一个朋友圈里，判断有多少个朋友圈。
-方法一：不相交集类。 创建N个集类，每个同学在一个集类里，若m[i][j]==1，那么将i所在的集类合并为一个集类。最后，集类的个数即为朋友圈的数目。
+方法一：不相交集类。 
+创建N个集类，每个同学在一个集类里，若m[i][j]==1，那么将i所在的集类合并为一个集类。最后，集类的个数即为朋友圈的数目。
 注意因为m[i][j]=m[j][i]，又m[i][i]=1，所以从对角线开始，搜索左下角矩阵即可。
-方法二：dfs，从同学0开始，若有朋友arr,则顺序搜索arr中个每个同学的朋友,搜索到的矩阵元素均在一个朋友圈中。
+方法二：DFS & bool型记录表.
+对每个同学，如果有朋友，那么搜索朋友的朋友。边界条件，使用一个bool变量记录每个同学是否已在某个朋友圈里。注意朋友圈只有一个同学的情况，也是一个朋友圈。
+对每个同学，搜索一列确定其朋友，再去搜索朋友所在的列，即朋友的朋友。先判断该同学是否在某个朋友圈中，即bool数组的真假，若不在，那么朋友圈数+1，遍历该列确定其朋友，并把所有朋友设为在某个
+朋友圈中。
+方法三： DFS&修改矩阵
+同方法二。但是不利用bool型数组保存每个同学是否在某个朋友圈中，而是利用m[i][i]的0-1来代表是否在某个朋友圈中，从搜索开始，如从i开始按列
+搜索，有m[j][i]=1，那么修改m[i][j]=m[j][i]=0防止重复访问，同时将m[j][j]设为0，标志同学j已在某个朋友圈中。这样遍历m[i][i]，若为1，将所有其同学
+以及同学的同学设为0，统计m[i][i]中1个个数即可。
 */
 
 //方法一：不相交集类
@@ -57,5 +65,64 @@ public:
         for(int i=0;i<n;i++)
             res.emplace(union_find[i]);
         return res.size();
+    }
+};
+
+//方法二 DFS & bool记录表
+class Solution {
+public:
+    int findCircleNum(vector<vector<int>>& m) {
+        int n=m.size();
+        if(n==0)
+            return 0;
+        int res=0;
+        vector<bool> find(n,false);//记录该同学是否已在某个朋友圈中
+        for(int i=0;i<n;i++)
+            if(!find[i])//该同学未在朋友圈
+            {
+                res++;//朋友圈数目+1
+                dfs(m,i,find);//将所有该同学的朋友（的朋友）设为在某个朋友圈中。
+            }
+        return res;
+    }
+    void dfs(vector<vector<int>>& m,int k,vector<bool>& find)
+    {
+        int n=m.size();
+        for(int i=0;i<n;i++)//先序搜索该列
+            if(m[i][k]==1&&!find[i])//k和i是朋友关系，且i未在某个朋友圈中
+            {
+                find[k]=true;//设i已在某个朋友圈中
+                dfs(m,i,find);//搜索i的朋友
+            }
+    }
+};
+
+//方法三 DFS & 修改矩阵
+class Solution {
+public:
+    int findCircleNum(vector<vector<int>>& m) {
+        if(m.empty())
+            return 0;
+        int n=m.size();;
+        int res=0;
+        for(int i=0;i<n;i++)
+            if(m[i][i]==1)//m[i][i]代表i同学是否在某个朋友圈中
+            {
+                res++;//如果为1说明不在，因此i和所有i的同学在一个新朋友圈中 朋友圈+1
+                dfs(m,i);//将所有i的同学设为0
+            }
+        return res;
+    }
+    void dfs(vector<vector<int>>& m,int k)
+    {
+        int n=m.size();
+        for(int i=0;i<n;i++)
+            if(m[i][k]==1)//搜索k的同学 
+            {
+                m[i][k]=0;//将k的同学关系设为0 这样不会再搜索回来
+                m[k][i]=0;//k的同学关系设为0 
+                m[i][i]=0;//记录k的同学i为已在某个朋友圈中
+                dfs(m,i);//搜索k的同学已经k的同学i的同学
+            }
     }
 };
