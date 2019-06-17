f0ed68e0c2bb5cf80a11b430c2b3bbb7766874f8
侯安康 <1611506326@qq.com>
2019-06-17 16:17:14 +0800

    Update 28. 实现strStr().cpp

--- 数组与矩阵/28. 实现strStr().cpp
+++ 数组与矩阵/28. 实现strStr().cpp
@@ -38,3 +38,43 @@ public:
 };
 
 //方法二 KMP
+class Solution {
+public:
+    int strStr(string haystack, string needle) {
+        if(needle.empty())
+            return 0;
+        if(haystack.empty())
+            return -1;
+        int sizeN=needle.size();
+        vector<int>next(sizeN+1);
+        int i=0,j=-1;
+        next[0]=-1;
+        while(i<sizeN)//构建next匹配表也就是needle字符串对自身匹配的过程
+        {
+            if(j==-1||needle[i]==needle[j])
+            {
+                i++;
+                j++;
+                next[i]=j;
+            }
+            else
+                j=next[j];
+        }
+        int sizeH=haystack.size();
+        i=0,j=0;
+        while(i<sizeH&&j<sizeN)
+        {
+            if(j==-1||haystack[i]==needle[j])
+            {
+                i++;
+                j++;
+            }
+            else
+                j=next[j];
+        }
+        if(j==sizeN)//needle最后一个字符也匹配 第一次匹配成功
+            return i-j;
+        else
+            return -1;
+    }
+};
