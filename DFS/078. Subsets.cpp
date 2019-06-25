004feba1d5d3e38ec2bd997581051d90368b47cc
侯安康 <1611506326@qq.com>
2019-06-25 23:14:32 +0800

    Update 078. Subsets.cpp

--- DFS/078. Subsets.cpp
+++ DFS/078. Subsets.cpp
@@ -15,6 +15,8 @@ Output:
   []
 ]
 */
+
+//使用副本进行合法dfs 不需要回溯 但是空间复杂度高
 class Solution {
 public:
     vector<vector<int>> subsets(vector<int>& nums)
@@ -38,5 +40,28 @@ public:
         int size=nums.size();
         for(int i=index+1;i<size;i++)//向后dfs 当index超出数组边界后停止
             dfs(res,nums,temp,i);
+    }
+};
+
+//使用引用进行dfs 需要回溯 时间复杂度空间复杂度俱佳
+class Solution {
+public:
+    int size=0;//数组大小 使用全局变量减小空间复杂度
+    vector<vector<int>> res;//保存 结果
+    vector<vector<int>> subsets(vector<int>& nums) {
+        size=nums.size();//数组大小赋值
+        res.push_back({});//添加空集 空集是任何集合的子集
+        vector<int> temp;//暂存变量
+        for(int i=0;i<size;i++)//将数组当前元素作为第一个选取的元素 对其后的元素dfs 防止重复
+            dfs(temp,nums,i);
+        return res;
+    }
+    void dfs(vector<int>& temp,vector<int>& nums,int index)
+    {
+        temp.push_back(nums[index]);//合法index 将nums[index]放入结果
+        res.push_back(temp);//因为集合个数从1开始 所以所有结果当合法
+        for(int i=index+1;i<size;i++)//对其后的索引dfs
+            dfs(temp,nums,i);
+        temp.pop_back();//尾回溯 退出时删除当前添加的元素
     }
 };
