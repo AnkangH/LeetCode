10380ce55761d72fe059a725265dcabb7dd987c9
侯安康 <1611506326@qq.com>
2019-07-05 12:07:01 +0800

    Update and rename 110. Balanced Binary Tree to 110. 平衡二叉树.cpp

---
+++ 数据结构/树/110. 平衡二叉树.cpp
@@ -0,0 +1,58 @@
+/*
+给定一个二叉树，判断它是否是高度平衡的二叉树。
+本题中，一棵高度平衡二叉树定义为：
+一个二叉树每个节点 的左右两个子树的高度差的绝对值不超过1。
+示例 1:
+给定二叉树 [3,9,20,null,null,15,7]
+
+    3
+   / \
+  9  20
+    /  \
+   15   7
+返回 true 。
+示例 2:
+给定二叉树 [1,2,2,3,3,null,null,4,4]
+
+       1
+      / \
+     2   2
+    / \
+   3   3
+  / \
+ 4   4
+返回 false 。
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/balanced-binary-tree
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+*/
+
+
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    bool res=true;
+    bool isBalanced(TreeNode* root) {
+        dfs(root);
+        return res;
+    }
+    int dfs(TreeNode* root)
+    {
+        if(root==nullptr)
+            return 0;
+        int l=dfs(root->left);
+        int r=dfs(root->right);
+        if(abs(r-l)>1)
+            res=false;
+        return max(l,r)+1;//从最深层的递归往前推 所以要用返回值
+    }
+};
