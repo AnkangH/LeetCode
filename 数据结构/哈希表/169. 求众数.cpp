7b852daf38ff1758a0bf1f5e74bc5a0034e88602
侯安康 <1611506326@qq.com>
2019-07-09 16:23:40 +0800

    Create 169. 求众数.cpp

---
+++ 数据结构/哈希表/169. 求众数.cpp
@@ -0,0 +1,30 @@
+/*
+给定一个大小为 n 的数组，找到其中的众数。众数是指在数组中出现次数大于 ⌊ n/2 ⌋ 的元素。
+你可以假设数组是非空的，并且给定的数组总是存在众数。
+示例 1:
+输入: [3,2,3]
+输出: 3
+示例 2:
+输入: [2,2,1,1,1,2,2]
+输出: 2
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/majority-element
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+*/
+
+
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        unordered_map<int,int> m;
+        int n=nums.size();//总的个数
+        n=n>>1;//n/2
+        for(auto p:nums)//遍历数组
+        {
+            m[p]++;//当前数值出现次数+1
+            if(m[p]>n)//是众数
+                return p;//返回
+        }
+        return -1;//无众数
+    }
+};
