075cb8fc381641979d42d205f67c07e12d6717a4
侯安康 <1611506326@qq.com>
2019-07-22 13:53:35 +0800

    Create 284. 顶端迭代器.cpp

---
+++ 数组与矩阵/284. 顶端迭代器.cpp
@@ -0,0 +1,57 @@
+/*
+给定一个迭代器类的接口，接口包含两个方法： next() 和 hasNext()。设计并实现一个支持 peek() 操作的顶端迭代器 -- 
+其本质就是把原本应由 next() 方法返回的元素 peek() 出来。
+示例:
+假设迭代器被初始化为列表 [1,2,3]。
+调用 next() 返回 1，得到列表中的第一个元素。
+现在调用 peek() 返回 2，下一个元素。在此之后调用 next() 仍然返回 2。
+最后一次调用 next() 返回 3，末尾元素。在此之后调用 hasNext() 应该返回 false。
+进阶：你将如何拓展你的设计？使之变得通用化，从而适应所有的类型，而不只是整数型？
+*/
+
+
+// Below is the interface for Iterator, which is already defined for you.
+// **DO NOT** modify the interface for Iterator.
+
+class Iterator {
+    struct Data;
+	Data* data;
+public:
+	Iterator(const vector<int>& nums);
+	Iterator(const Iterator& iter);
+	virtual ~Iterator();
+	// Returns the next element in the iteration.
+	int next();
+	// Returns true if the iteration has more elements.
+	bool hasNext() const;
+};
+
+
+class PeekingIterator : public Iterator {
+public:
+	PeekingIterator(const vector<int>& nums) : Iterator(nums) {
+	    // Initialize any member here.
+	    // **DO NOT** save a copy of nums and manipulate it directly.
+	    // You should only use the Iterator interface methods.
+	}
+
+    // Returns the next element in the iteration without advancing the iterator.
+	int peek() {
+        if(hasNext())
+        {
+            auto p=*this;//使用当前指针的副本 不要移动迭代器
+            return p.next();
+        }
+        return -1;
+	}
+
+	// hasNext() and next() should behave the same as in the Iterator interface.
+	// Override them if needed.
+	int next() {
+        return Iterator::next();//使用父类的方法即可
+	}
+
+	bool hasNext() const {
+        return Iterator::hasNext();//使用父类的方法即可
+	}
+};
