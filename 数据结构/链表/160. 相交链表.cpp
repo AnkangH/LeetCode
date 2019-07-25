5515a3909e6960a7d69b84e1fb10ead318aeb710
侯安康 <1611506326@qq.com>
2019-07-25 19:10:29 +0800

    Update 160. 相交链表.cpp

--- 数据结构/链表/160. 相交链表.cpp
+++ 数据结构/链表/160. 相交链表.cpp
@@ -5,9 +5,6 @@
 在返回结果后，两个链表仍须保持原有的结构。
 可假定整个链表结构中没有循环。
 程序尽量满足 O(n) 时间复杂度，且仅用 O(1) 内存。
-来源：力扣（LeetCode）
-链接：https://leetcode-cn.com/problems/intersection-of-two-linked-lists
-著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
 */
 
 
