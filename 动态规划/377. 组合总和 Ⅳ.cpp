e39657b9d15d1860d1799e16703a0b410a03964b
侯安康 <1611506326@qq.com>
2019-07-28 00:22:14 +0800

    Update 377. 组合总和 Ⅳ.cpp

--- 动态规划/377. 组合总和 Ⅳ.cpp
+++ 动态规划/377. 组合总和 Ⅳ.cpp
@@ -18,6 +18,7 @@ target = 4
 */
 
 
+//方法1 自下向上递推
 class Solution {
 public:
     int combinationSum4(vector<int>& nums, int target) {
@@ -35,5 +36,34 @@ public:
                     dp[i]+=dp[i-p];//dp[i]=sum(dp[i-p]) p=nums[0]-nums[n-1],p<=i
                 }
         return dp[target];
+    }
+};
+
+
+//方法2 自上向下递推 使用备忘录
+class Solution {
+public:
+    unordered_map<int,int>m;//备忘录 若dp[i]已存在 直接查表 避免递归的重复求取相同值
+    int combinationSum4(vector<int>& nums, int target) {
+        return dfs(nums,target);
+    }
+    int dfs(vector<int>&nums,int target)
+    {
+        if(target==0)
+            return 1;//dp[0]=1;
+        int res=0;//当前target的组合数
+        for(auto p:nums)//遍历数组
+            if(p<=target)//dp[target-p]合法
+            {
+                if(m.count(target-p)==0)//dp[i]未求取
+                {
+                    int cur=dfs(nums,target-p);//求dp[i]
+                    m[target-p]=cur;//放入备忘录中
+                    res+=cur;//dp[target]=sum(dp(target-p));
+                }
+                else
+                    res+=m[target-p];//dp[i]已求取 查表
+            }
+        return res;
     }
 };
