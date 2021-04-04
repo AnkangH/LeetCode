18d791a2fe32695d95d8d9d39cb9bb4cb3c899c9
侯安康 <1611506326@qq.com>
2021-04-04 11:00:06 +0800

    Update 001. 两数之和.cpp

--- 数据结构/哈希表/001. 两数之和.cpp
+++ 数据结构/哈希表/001. 两数之和.cpp
@@ -24,3 +24,35 @@ public:
         return {};
     }
 };
+
+// 2021/4/4复习
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        //哈希表查找 时间复杂度o(n)
+        map<int, int> exists;
+        vector<int> result;
+        // 建立哈希表 处理多个相等元素=target/2的边界情况
+        for(int i=0;i <nums.size();i++){
+            auto itr = exists.find(target-nums[i]);
+            if(itr!=exists.end()){
+                return vector<int> {i, itr->second};
+            }
+            exists.insert(pair<int, int>(nums[i],i));
+        }
+        //下面的遍历也可以放入上面的遍历中 做优化 实际上 如果a+b=target那么无论a b谁在前不影响
+        //查a时 b不在， 也可以在查b时 a在这种情况找到该解
+        // 对每个元素 哈希表查找target-num
+//         for(int i=0;i<nums.size();i++){
+//             auto itr=exists.find(target-nums[i]);
+//             if(itr!=exists.end()){
+//                 return vector<int> {i, itr->second};
+//             }
+//         }
+        return result;
+    }
+};
+/*  1. pair<int, int> a(1,2); 代表初始化时传入的参数 因为pair的构造函数接受两个参数
+        pair<int, int> a{1, 2}; 列表初始化
+    2. stl相关数据结构的操作 迭代器itr 遍历写法等
+*/
