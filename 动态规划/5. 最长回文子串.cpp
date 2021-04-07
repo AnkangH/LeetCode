9a4cc3ecf853097965d7a36342cf3225d4444f33
侯安康 <1611506326@qq.com>
2021-04-07 13:31:56 +0800

    Update 5. 最长回文子串.cpp

--- 动态规划/5. 最长回文子串.cpp
+++ 动态规划/5. 最长回文子串.cpp
@@ -45,3 +45,51 @@ public:
         return s.substr(start,end-start+1);//返回最大长度的回文字符串
     }
 };
+
+
+//2021.4.7
+/*
+    1.dp[i][i]的遍历可以放到一个遍历流程里 不需单独处理
+*/
+class Solution {
+public:
+    string longestPalindrome(string s) {
+        int n =s.size();
+        vector<vector<bool>> dp(n, vector<bool>(n, false));
+        int start=0, end=0;
+        // 判断s(i,j)需先判断s(i+1,j-1) 
+        //所以 i是递减的 先知道s(i+1, )才能知道s(i)
+        for(int i=n-1;i>=0;i--)
+            for(int j=i;j<n;j++){
+                //dp[i][i]单个字符为回文串
+                if(j==i){
+                    dp[i][j]=true;
+                    if(j-i>end-start){
+                        end=j;
+                        start=i;
+                    }
+                }
+                // i和j相邻 字符相同为回文串
+                else if(j-i==1){
+                    if(s[j]==s[i]){
+                        dp[i][j]=true;
+                        if(j-i>end-start){
+                            end=j;
+                            start=i;
+                        }   
+                    }
+                }
+                //否则 i (i+1 j-1) j 若中间的s(i+1,j-1)为回文串 且s[i]==s[j] 则s(i,j)为回文串
+                else{
+                    if(dp[i+1][j-1] && s[j]==s[i]){
+                        dp[i][j]=true;
+                        if(j-i>end-start){
+                            end=j;
+                            start=i;
+                        }  
+                    }
+                }
+            }
+        return s.substr(start, end-start+1);
+    }
+};
